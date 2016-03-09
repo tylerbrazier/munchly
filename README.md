2d4bd591de4a835aecf4ac072c5619d41448d528
Tyler Brazier <tyler@tylerbrazier.com>
2016-03-08 18:28:57 -0600

    Initial commit; host static test page with express

---
+++ README.md
@@ -0,0 +1,12 @@
+Eat Fargo
+=========
+
+Install
+-------
+
+    npm install
+
+Run
+---
+
+    node index.js
---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+const PORT = 8080
+
+app.use(express.static('web'))
+
+app.listen(PORT, () => console.log('Listening on %d', PORT))
