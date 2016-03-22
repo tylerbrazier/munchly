da952014fe8d0c1eb8f68c67d326de4580ee50e5
Tyler Brazier <tyler@tylerbrazier.com>
2016-03-22 01:03:22 -0400

    Clicking a category populates simple items list

--- web/resources/js/menu.js
+++ web/resources/js/menu.js
@@ -1,17 +1,67 @@
 'use strict'
-$(() => {
 
+let categoriesUl, meatDiv
+
+$(() => {
+  categoriesUl = $('#categories')
+  meatDiv = $('#meat')
+
+  populateCategoriesFromAjax()
+})
+
+function populateCategoriesFromAjax() {
   $.ajax({
     url: '/api/categories',
     dataType: 'json',
-    success: (data) => {
-      let ul = $('#categories')
-      data.forEach( (c) => ul.append(`<li><a href="#">${c.name}</a></li>`) )
-    },
+    success: appendCategoryListItems,
     error: (jqXHR, textStatus, err) => {
       console.error(jqXHR.responseText)
       alert(jqXHR.responseText)
     }
   })
+}
 
-})
+function appendCategoryListItems(jsonData) {
+  jsonData.forEach( (c) => {
+    let li = $(`<li><a href="#">${c.name}</a></li>`)
+    li.click((event) => {
+      event.preventDefault()
+      populateMenuItemsFromAjax(c.id)
+    })
+    categoriesUl.append(li)
+  })
+}
+
+function populateMenuItemsFromAjax(categoryId) {
+  $.ajax({
+    url: `/api/categories/${categoryId}`,
+    dataType: 'json',
+    success: setMenuListItems,
+    error: (jqXHR, textStatus, err) => {
+      console.error(jqXHR.responseText)
+      alert(jqXHR.responseText)
+    }
+  })
+}
+
+function setMenuListItems(jsonData) {
+  meatDiv.empty()
+  jsonData.items.forEach( (i) => {
+    let panel = getMenuItemHtmlTemplate(i)
+    meatDiv.append(panel)
+  })
+}
+
+function getMenuItemHtmlTemplate(item) {
+  return `\
+  <div class="col-md-4">
+    <div class="panel panel-default">
+      <div class="panel-body">
+        <img class="img-responsive" src="${item.image}">
+      </div>
+      <div class="panel-footer">
+        ${item.name}
+      </div>
+    </div>
+  </div>`
+}
