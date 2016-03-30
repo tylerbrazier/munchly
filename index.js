d1eb5d42bcd94ef4ec69a40d44bec588d78f45ea
Tyler Brazier <tyler@tylerbrazier.com>
2016-03-29 22:54:10 -0400

    Use conf instead of defining defaults in index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+'use strict'
 const express = require('express')
 const mongoose = require('mongoose')
-const PORT = 8080
+let conf = require('./.default.conf')
 
-mongoose.connect('mongodb://localhost/test')
+try {
+  conf = Object.assign(conf, require('./conf'))
+} catch (err) {
+  console.warn('No conf defined; using defaults')
+}
+console.log(conf)
+
+mongoose.connect(conf.db)
 const db = mongoose.connection
 
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -13,5 +21,5 @@ db.once('open', () => {
   app.use(express.static('web'))
   app.use('/api', require('./routes/api'))
 
-  app.listen(PORT, () => console.log('Listening on %d', PORT))
+  app.listen(conf.port, () => console.log('Listening on %d', conf.port))
 })
