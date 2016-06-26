184ab96622d00fd5a15d064aa14d4324eb2b5694
Tyler Brazier <tyler@tylerbrazier.com>
2016-06-26 16:53:39 -0500

    Add POST, PUT, and DELETE apis to category

--- routes/categories.js
+++ routes/categories.js
@@ -1,13 +1,27 @@
 const router = require('express').Router()
 const Category = require('../models/category')
+const bodyParser = require('body-parser')
 
 module.exports = router
+
+router.use(bodyParser.urlencoded({ extended: true }))
 
 router.get('/', (req, res, next) => {
   Category.find((err, categories) => {
     if (err)
       return next(err)
     res.json(categories)
+  })
+})
+
+router.post('/', (req, res, next) => {
+  Category.create({
+    name: req.body.name,
+    items: req.body.items ? req.body.items.split(',') : [],
+  }, (err, category) => {
+    if (err)
+      return next(err)
+    res.json(category)
   })
 })
 
@@ -28,4 +42,22 @@ router.param('id', (req, res, next, id) => {
 
 router.get('/:id', (req, res, next) => {
   res.json(req.category)
+})
+
+router.put('/:id', (req, res, next) => {
+  req.category.name = req.body.name
+  req.category.items = req.body.items ? req.body.items.split(',') : []
+  req.category.save((err, category, nUpdated) => {
+    if (err)
+      return next(err)
+    res.json(category)
+  })
+})
+
+router.delete('/:id', (req, res, next) => {
+  req.category.remove((err, category) => {
+    if (err)
+      return next(err)
+    res.json(category)
+  })
 })
