99678655e82131de7bcc81e1135e85239d19a263
Tyler Brazier <tyler@tylerbrazier.com>
2016-06-25 20:58:12 -0500

    Add rest api for items

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,9 @@ const router = require('express').Router()
 
 module.exports = router
 
-router.use('/categories', require('./categories'))
 router.use('/menu', require('./menu'))
+router.use('/categories', require('./categories'))
+router.use('/items', require('./items'))
 router.use('/feedback', require('./feedback'))
 router.use('/', require('../middleware/404'))
 router.use('/', require('../middleware/err-handler').forJson)
---
+++ routes/items.js
@@ -0,0 +1,45 @@
+const router = require('express').Router()
+const Item = require('../models/item')
+const bodyParser = require('body-parser')
+
+module.exports = router
+
+router.use(bodyParser.urlencoded({ extended: true }))
+
+router.get('/', (req, res, next) => {
+  Item.find((err, items) => {
+    if (err)
+      return next(err)
+    res.json(items)
+  })
+})
+
+router.post('/', (req, res, next) => {
+  Item.create({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    image: req.body.image,
+  }, (err, item) => {
+    if (err)
+      return next(err)
+    res.json(item)
+  })
+})
+
+router.param('id', (req, res, next, id) => {
+  Item.findOne({ _id: id }, (err, item) => {
+    if (err)
+      return next(err)
+    if (!item) {
+      res.status(404)
+      return next(new Error(`Item ${id} not found`))
+    }
+    req.item = item
+    next()
+  })
+})
+
+router.get('/:id', (req, res, next) => {
+  res.json(req.item)
+})
