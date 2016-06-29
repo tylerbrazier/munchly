ba21e9c3f62326aaef61bac4604e68dc5f76dc34
Tyler Brazier <tyler@tylerbrazier.com>
2016-06-29 00:31:11 -0400

    Split all.{css,js} into multiple files
    This is so that admin pages will be able to use specific files as needed.
    I haven't measured the performance difference between using one complete
    file vs multiple but I don't think it makes much difference at this
    point (hopefully).
    
    You can use `gulp clean` after this commit to clean up old files.

--- gulpfile.js
+++ gulpfile.js
@@ -3,53 +3,97 @@ const gulp = require('gulp')
 const babel = require('gulp-babel')
 const cleanCss = require('gulp-clean-css')
 const concat = require('gulp-concat')
-const merge = require('merge2')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
+const del = require('del')
 
 const resourcesDir = './web/resources'
 
 gulp.task('default', ['css', 'js'])
 
-gulp.task('css', css);
-function css() {
-  return gulp.src([
-    './node_modules/hamburgers/dist/hamburgers.css',
-    './node_modules/bootstrap/dist/css/bootstrap.css',
-    './node_modules/bootstrap-star-rating/css/star-rating.css',
-    './node_modules/bootstrap-star-rating/css/theme-krajee-uni.css',
-    `${resourcesDir}/css/*.css`,
-    './local/web/*.css',
-  ])
-  .pipe(concat('all.css'))
-  .pipe(gulp.dest(`${resourcesDir}/css/dist`))
-  .pipe(cleanCss())
-  .pipe(rename((path) => path.basename += '.min' ))
-  .pipe(gulp.dest(`${resourcesDir}/css/dist`))
+gulp.task('css', [
+  'css:custom',
+  'css:local',
+  'css:bootstrap',
+  'css:hamburgers',
+  'css:stars',
+  'css:krajee',
+])
+
+gulp.task('js', [
+  'js:custom',
+  'js:jquery',
+  'js:bootstrap',
+])
+
+gulp.task('clean', () => del(`${resourcesDir}/*/dist`))
+
+gulp.task('watch', () => {
+  cssCustom()
+  cssLocal()
+  jsCustom()
+  gulp.watch(`${resourcesDir}/js/*.js`, ['js:custom'])
+  gulp.watch('./local/web/*.css', ['css:local'])
+  gulp.watch(`${resourcesDir}/css/*.css`, ['css:custom'])
+})
+
+
+gulp.task('css:custom', cssCustom)
+function cssCustom() {
+  return gulp.src(`${resourcesDir}/css/*.css`)
+    .pipe(gulp.dest(`${resourcesDir}/css/dist`))
+    .pipe(cleanCss())
+    .pipe(rename((path) => path.basename += '.min' ))
+    .pipe(gulp.dest(`${resourcesDir}/css/dist`))
 }
 
-gulp.task('js', js)
-function js() {
+gulp.task('css:local', cssLocal)
+function cssLocal() {
+  return gulp.src('./local/web/*.css')
+    .pipe(concat('local.css'))
+    .pipe(gulp.dest(`${resourcesDir}/css/dist`))
+    .pipe(cleanCss())
+    .pipe(rename((path) => path.basename += '.min' ))
+    .pipe(gulp.dest(`${resourcesDir}/css/dist`))
+}
+
+gulp.task('css:bootstrap', () => {
+  return gulp.src('./node_modules/bootstrap/dist/css/bootstrap.min.css')
+    .pipe(gulp.dest(`${resourcesDir}/css/dist`))
+})
+gulp.task('css:hamburgers', () => {
+  return gulp.src('./node_modules/hamburgers/dist/hamburgers.min.css')
+    .pipe(gulp.dest(`${resourcesDir}/css/dist`))
+})
+gulp.task('css:stars', () => {
+  return gulp.src('./node_modules/bootstrap-star-rating/css/star-rating.min.css')
+    .pipe(gulp.dest(`${resourcesDir}/css/dist`))
+})
+gulp.task('css:krajee', () => {
+  return gulp.src('./node_modules/bootstrap-star-rating/css/theme-krajee-uni.min.css')
+    .pipe(gulp.dest(`${resourcesDir}/css/dist`))
+})
+
+
+gulp.task('js:custom', jsCustom)
+function jsCustom() {
   // need to handle errors so the watch task doesn't stop
   const b = babel({ presets: ['es2015'] })
   b.on('error', (err) => { console.error(err.message); b.end() })
 
-  return merge(
-    gulp.src('./node_modules/jquery/dist/jquery.js'),
-    gulp.src('./node_modules/bootstrap/dist/js/bootstrap.js'),
-    gulp.src('./node_modules/bootstrap-star-rating/js/star-rating.js'),
-    gulp.src(`${resourcesDir}/js/*.js`).pipe(b)
-  )
-  .pipe(concat('all.js'))
-  .pipe(gulp.dest(`${resourcesDir}/js/dist`))
-  .pipe(uglify())
-  .pipe(rename((path) => path.basename += '.min' ))
-  .pipe(gulp.dest(`${resourcesDir}/js/dist`))
+  return gulp.src(`${resourcesDir}/js/*.js`)
+    .pipe(b)
+    .pipe(gulp.dest(`${resourcesDir}/js/dist`))
+    .pipe(uglify())
+    .pipe(rename((path) => path.basename += '.min' ))
+    .pipe(gulp.dest(`${resourcesDir}/js/dist`))
 }
 
-gulp.task('watch', () => {
-  css()
-  js()
-  gulp.watch(`${resourcesDir}/js/*.js`, ['js'])
-  gulp.watch(`${resourcesDir}/css/*.css`, ['css'])
+gulp.task('js:jquery', () => {
+  return gulp.src('./node_modules/jquery/dist/jquery.min.js')
+    .pipe(gulp.dest(`${resourcesDir}/js/dist`))
+})
+gulp.task('js:bootstrap', () => {
+  return gulp.src('./node_modules/bootstrap/dist/js/bootstrap.min.js')
+    .pipe(gulp.dest(`${resourcesDir}/js/dist`))
 })
